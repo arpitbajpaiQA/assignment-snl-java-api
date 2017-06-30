46b079ee090f0ac8381cde1e6a411ae6499590f0
Arpit Bajpai <arpitbajpai@qainfotech.com>
2017-06-30 12:03:06 +0530

    Update TestBoard.java

--- test/java/TestBoard.java
+++ test/java/TestBoard.java
@@ -2,6 +2,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.UUID;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -9,74 +11,86 @@ import org.testng.annotations.Test;
 import com.qainfotech.tap.training.snl.api.Board;
 import com.qainfotech.tap.training.snl.api.BoardModel;
 import com.qainfotech.tap.training.snl.api.GameInProgressException;
+import com.qainfotech.tap.training.snl.api.InvalidTurnException;
 import com.qainfotech.tap.training.snl.api.MaxPlayersReachedExeption;
 import com.qainfotech.tap.training.snl.api.NoUserWithSuchUUIDException;
 import com.qainfotech.tap.training.snl.api.PlayerExistsException;
 
+/**
+ * 
+ * @author arpit
+ *
+ */
 public class TestBoard {
 	Board b;
 	BoardModel model;
 	UUID id;
 	Object obj;
-	JSONObject player = (JSONObject)obj;
+	JSONObject player = (JSONObject) obj;
 
 	@BeforeTest
 	public void load_the_Board() throws FileNotFoundException, UnsupportedEncodingException, IOException {
 		b = new Board();
-		id=b.getUUID();
-		b=new Board(id);
-		}
+		id = b.getUUID();
+		b = new Board(id);
+	}
 
 	@Test
-	public void adding_a_player()
+	public void adding_new_player()
 			throws IOException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption {
-		b.registerPlayer("Arpit");
-		b.registerPlayer("Ruchir");
+		b.registerPlayer("arpit");
+		b.registerPlayer("ruchir");
 
 		b.getData();
 	}
 
-	@Test
-	public void adding_new_player_should_throw_AlreadyPlayerExistsExcetion()
+	@Test(expectedExceptions = PlayerExistsException.class)
+	public void adding_new_player_should_throw_Already_PlayerExistsException()
 			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
 			MaxPlayersReachedExeption, IOException {
-		b.registerPlayer("Ruchir");
+		b.registerPlayer("ruchir");
 		b.getData();
 	}
 
-	@Test
+	@Test(expectedExceptions = MaxPlayersReachedExeption.class)
 	public void adding_new_player_should_throw_MaxPlayersReachedExeption()
 			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
 			MaxPlayersReachedExeption, IOException {
+		b.registerPlayer("Vishnu");
 		b.registerPlayer("himanshu");
-		b.registerPlayer("pandey");
-		b.registerPlayer("vishnu");
-	
+		b.registerPlayer("Pandey");
+
 		b.getData();
 	}
 
 	@Test
-	public void deleting_the_player_removes_player() throws FileNotFoundException, 
-	UnsupportedEncodingException, NoUserWithSuchUUIDException{
-		
+	public void deleting_the_player_removes_player()
+			throws FileNotFoundException, UnsupportedEncodingException, NoUserWithSuchUUIDException {
+
 		UUID uuid1 = (UUID) b.getData().getJSONArray("players").getJSONObject(2).get("uuid");
-		b.deletePlayer(uuid1);	
+		b.deletePlayer(uuid1);
 	}
-	
-	@Test
-	public void deleting_the_player_throws_NoUserWithSuchUUIDException() throws FileNotFoundException,
-	UnsupportedEncodingException, NoUserWithSuchUUIDException{
+
+	@Test(expectedExceptions = NoUserWithSuchUUIDException.class)
+	public void deleting_the_player_throws_NoUserWithSuchUUIDException()
+			throws FileNotFoundException, UnsupportedEncodingException, NoUserWithSuchUUIDException {
 		b.deletePlayer(id);
-		
+
 	}
-	
-		
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void roll_the_dice()
+			throws FileNotFoundException, UnsupportedEncodingException, JSONException, InvalidTurnException {
+
+		b.rollDice((UUID) ((JSONObject) b.getData().getJSONArray("players").get(0)).get("uuid"));
+
+	}
+
+	@Test(expectedExceptions = InvalidTurnException.class)
+	public void roll_the_dice_to_throw_InvalidTurnException()
+			throws FileNotFoundException, UnsupportedEncodingException, JSONException, InvalidTurnException {
+		b.rollDice(id);
+
+	}
+
 }
